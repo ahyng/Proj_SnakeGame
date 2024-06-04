e476e68da384193adab6ef1fee64b0609a28ae48
ahyng <136292055+ahyng@users.noreply.github.com>
2024-06-04 10:10:32 +0900

    tail방향으로 이동시 gameover

--- handle.cpp
+++ handle.cpp
@@ -7,21 +7,22 @@ void handleInput() {
         // 키 입력 받아 스네이크의 방향 업데이트
         ch = getch();
         switch (ch) {
+            // tail 방향으로 이동시 gameover
             case KEY_UP:
-                if (direction != KEY_DOWN)
-                    direction = KEY_UP;
+                if (direction == KEY_DOWN) {game_over = true;}
+                    else {direction = KEY_UP;}
                 break;
             case KEY_DOWN:
-                if (direction != KEY_UP)
-                    direction = KEY_DOWN;
+                if (direction == KEY_UP) {game_over = true;}
+                    else {direction = KEY_DOWN;}
                 break;
             case KEY_LEFT:
-                if (direction != KEY_RIGHT)
-                    direction = KEY_LEFT;
+                if (direction == KEY_RIGHT) {game_over = true;}
+                    else {direction = KEY_LEFT;}
                 break;
             case KEY_RIGHT:
-                if (direction != KEY_LEFT)
-                    direction = KEY_RIGHT;
+                if (direction == KEY_LEFT) {game_over = true;}
+                    else {direction = KEY_RIGHT;}
                 break;
         }
     }
